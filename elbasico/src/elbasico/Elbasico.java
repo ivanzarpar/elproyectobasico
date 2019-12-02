badef611191838e8d40e81bb98e6275bdc3cb2d9
ivan_ <ivan_@LAPTOP-LVTM11G4>
2019-12-02 19:16:10 +0100

    trabajo en la rama logicos

--- elbasico/src/elbasico/Elbasico.java
+++ elbasico/src/elbasico/Elbasico.java
@@ -11,11 +11,54 @@ package elbasico;
  */
 public class Elbasico {
 
-    /**
-     * @param args the command line arguments
+   /**
+
+     * Método para mostrar ejemplos de tipo lógico.
+
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static void logicos() {
+
+
+
+        //Estos son los tipos básicos
+
+        boolean bueno;
+
+        int a;
+
+        int b;
+
+
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+
+        bueno = true;
+
+        a = 5;
+
+        b = 4;
+
+
+
+        if (a > b){
+
+        //Mostrar en pantalla el contendido siempre que se cumpla la condición.
+
+        
+
+       
+
+        System.out.println(bueno);
+
     }
-    
+
+    }
+
+     public static void main(String[] args) {
+
+        logicos();  // Llamada al método
+
+    }
+
 }
